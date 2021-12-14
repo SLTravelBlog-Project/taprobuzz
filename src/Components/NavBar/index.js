d5826da0eab7c9c5b126c187bdc6bab6a4fc4907
Madhusanka331 <cmadhusanka29@gmail.com>
2021-12-14 23:41:05 +0530

    nav bar

---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa';
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks
+} from './NavbarElements';
+
+const Navbar = () => {
+    return (
+        <>
+         <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>Taprobuzz</NavLogo>
+                <MobileIcon>
+                    <FaBars/>
+                </MobileIcon>
+
+             
+             <NavMenu>
+                <NavItem>
+                 <NavLinks to="/articals" >Articals</NavLinks>
+                </NavItem>
+                <NavItem>
+                 <NavLinks to="/things" >Things To Do In Sri Lanka</NavLinks>
+                </NavItem>
+                <NavItem>
+                 <NavLinks to="/hotels" >Resorts</NavLinks>
+                </NavItem>
+                <NavItem>
+                 <NavLinks to="/gallery" >Gallery</NavLinks>
+                </NavItem>
+                <NavItem>
+                 <NavLinks to="/about" >About Us</NavLinks>
+                </NavItem>
+                <NavItem>
+                 <NavLinks to="/Contact" >Contact Us</NavLinks>
+                </NavItem> 
+              </NavMenu>
+             </NavbarContainer>
+         </Nav>
+        </>
+    )
+}
+
+export default Navbar
+
---
+++ src/pages/things.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Things = () => {
+    return (
+        <div
+         style={{
+             display:'flex', 
+             justifyContent:'center', 
+             alignItems:'center', 
+             height:'90vh' 
+            }} 
+        >
+            <h1>Things To Do in Sri Lanka</h1>
+        </div>
+    )
+}
+
+export default Things;
